1fe302bae8ed1d5e6ab84f20e7fcdaad46958ee9
o1egs <tankistonline21@gmail.com>
2024-03-12 10:01:42 +0300

    ahahaha

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CI/CD KAIF
+          CI/CD KAIF REAL
         </a>
       </header>
     </div>
